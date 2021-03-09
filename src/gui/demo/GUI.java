b1e990e05fb1d98adc975f2216b1b4742124075f
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-03-09 13:35:28 +0530

    GUI Project WIP

---
+++ src/gui/demo/GUI.java
@@ -0,0 +1,16 @@
+package gui.demo;
+
+import javax.swing.JFrame;
+
+public class GUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module guiProject {
+	requires java.desktop;
+}
