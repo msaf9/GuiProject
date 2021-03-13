26624fc13b4e83d8922e10eaa41be92f856d1d52
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-03-13 16:56:34 +0530

    WIP: Login page implementation

--- src/gui/demo/GUI.java
+++ src/gui/demo/GUI.java
@@ -1,16 +1,71 @@
 package gui.demo;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class GUI implements ActionListener {
+	private static JLabel userLabel;
+	private static JTextField userText;
+	private static JLabel passwordLabel;
+	private static JPasswordField passwordText;
+	private static JButton button;
+	private static JLabel success;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		JPanel panel = new JPanel();
 		JFrame frame = new JFrame();
-		frame.setSize(500, 500);
+		frame.setSize(350, 250);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+
+		panel.setLayout(null);
+		userLabel = new JLabel("User");
+		userLabel.setBounds(10, 20, 80, 25);
+		panel.add(userLabel);
+
+		userText = new JTextField();
+		userText.setBounds(100, 20, 165, 25);
+		panel.add(userText);
+
+		passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(10, 50, 80, 25);
+		panel.add(passwordLabel);
+
+		passwordText = new JPasswordField();
+		passwordText.setBounds(100, 50, 165, 25);
+		panel.add(passwordText);
+
+		button = new JButton("Login");
+		button.setBounds(10, 80, 80, 25);
+		button.addActionListener(new GUI());
+		panel.add(button);
+
+		success = new JLabel("");
+		success.setBounds(10, 110, 300, 25);
+		panel.add(success);
+
 		frame.setVisible(true);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String user = userText.getText();
+		String password = passwordText.getText();
+//		System.out.println(user + ", " + password);
+		
+		if(user.equals("admin") && password.equals("admin")) {
+			success.setText("");
+			success.setText("Login successful!");
+		}else {
+			success.setText("");
+			success.setText("Login unsuccessful!!");
+		}
+	}
 }
